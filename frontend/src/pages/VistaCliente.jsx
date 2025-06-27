4c1c6acf627f5dd42d60ba9076ae01452ac654d8
ibermego <ibermego@gmail.com>
2025-06-27 14:53:20 +0200

    comment atrasado

--- frontend/src/pages/VistaCliente.jsx
+++ frontend/src/pages/VistaCliente.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './taberna.css';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://proyectofinal-backend3.onrender.com";
-// const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+// const API_BASE_URL = import.meta.env.VITE_API_URL || "https://proyectofinal-backend3.onrender.com";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 function VistaCliente() {
   const [pedido, setPedido] = useState(null);
